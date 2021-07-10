f780132b718f67527d834f7c53972e385111e18c
Todd Mullen <ToddJ.Mullen@gmail.com>
2021-07-10 18:17:06 -0400

    ExpenseItem, Remove/disable stateful behavior

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
 import './ExpenseItem.css';
 
@@ -10,23 +10,23 @@ function ExpenseItem(props){
 
     // create state for title & extract into local const for use in the jsx template (also cannot be assigned to [that is wrong use])
     // Also extract/name the matching setter for it (which we also don't want to overwrite)
-    const [title, setTitle] = useState(props.title);// always returns 2 elements, current value & method to set it, names are by convention
+    // const [title, setTitle] = useState(props.title);// always returns 2 elements, current value & method to set it, names are by convention
 
-    const onClick = click => {
-        // title = "asd";// wrong & fails bc it's a const
-        setTitle(`test`);// correct & triggers ui update
-        console.log("onClick/", title, click );
-        console.log("onClick/ og data:", props.data );
-    }
+    // const onClick = click => {
+    //     // title = "asd";// wrong & fails bc it's a const
+    //     setTitle(`test`);// correct & triggers ui update
+    //     console.log("onClick/", title, click );
+    //     console.log("onClick/ og data:", props.data );
+    // }
 
     return (
         <Card className="expense-item">
             <ExpenseDate data={props.data} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.data.title}</h2>
                 <div className="expense-item__price">${props.data.price}</div>
             </div>
-            <button onClick={onClick} >Change Title</button>
+            {/* <button onClick={onClick} >Change Title</button> */}
         </Card>
     )
 }
