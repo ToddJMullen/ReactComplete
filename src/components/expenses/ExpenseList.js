e9c14a99abe635d2bb3653c2f976da72e2df2c06
Todd Mullen <ToddJ.Mullen@gmail.com>
2021-07-10 18:44:34 -0400

    ExpenseList, Make output list dynamic with input

--- src/components/expenses/ExpenseList.js
+++ src/components/expenses/ExpenseList.js
@@ -9,7 +9,7 @@ import { useState } from 'react';
 function ExpenseList(props){
 	console.log("ExpenseList/ Props:", props );
 
-	const DEFAULT_FILTER_YEAR = (new Date).getFullYear()
+	const DEFAULT_FILTER_YEAR = (new Date()).getFullYear()
   ,expAry = props.expenseAry
   ,[filterYear, setFilterYear] = useState( DEFAULT_FILTER_YEAR )
   ,onFilterYear = year => {
@@ -22,11 +22,7 @@ function ExpenseList(props){
 	return (// don't know how to iterate yet
 	<Card className="expense-list">
 		<ExpensesFilter filterYear={filterYear} onFilterYear={onFilterYear} />
-		<ExpenseItem data={expAry[0]} />
-		<ExpenseItem data={expAry[1]} />
-		<ExpenseItem data={expAry[2]} />
-		<ExpenseItem data={expAry[3]} />
-		<ExpenseItem data={expAry[4]} />
+		{expAry.map( exp => <ExpenseItem data={exp} /> )}
 		</Card>
 	)
 }
